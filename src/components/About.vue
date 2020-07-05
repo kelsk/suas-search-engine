f9ae5a1e04f3df9d7b634ce521a2bba278ff2ffd
Kelsey Krippaehne <kelseykripp@gmail.com>
2020-07-05 16:06:16 -0700

    Initial code added

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <p>
+    The Shut Up & Sew search engine was created to quickly and easily search the websites of all the vendors from @pinkmimosabyjacinta's vendor list. These vendors have been vetted by Jacinta or by trusted followers as stores owned and operated by BIPOC and allies. 
+    </p>
+    <p>
+    The search engine is created and maintained by kelsk. Please submit any bugs or feature requests through <a v-bind:href="urls.github" alt="github codebase">Github</a>. Feel free to contribute!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data: ()=>{
+    return {
+      urls: {
+        github: "https://github.com/kelsk/suas-search-engine"
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <div>
+      <h1>
+        <a href="/">
+        Shut Up & Sew search engine
+        </a>
+      </h1>
+      <h2>
+        Search all the vetted websites from <a v-bind:href="links.pinkmimosabyjacinta.url" v-bind:alt="links.pinkmimosabyjacinta.alt">
+        @pinkmimosabyjacinta</a>'s 
+        <a v-bind:href="links.spreadsheet.url" v-bind:alt="links.spreadsheet.alt">
+        Shut Up & Sew vendor list
+        </a>
+      </h2>
+    </div>
+    <div>
+      <button v-on:click="toggleAbout" class="about-btn mdc-button mdc-button--raised" id="about">
+        <span class="mdc-button__label">
+        About
+        </span>
+        </button>
+    </div>
+    <div v-if="!aboutHidden" v-on:click="toggleAbout" class="about-wrapper">
+    </div>
+      <About v-if="!aboutHidden"></About>
+</header>
+</template>
+
+<script>
+import About from './About.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    About,
+  },
+  data: function () {
+    return {
+      aboutHidden: true,
+      links: {
+        pinkmimosabyjacinta: {
+          url: process.env.VUE_APP_PMBJ_IG,
+          alt: "pinkmimosabyjacinta instagram"
+        },
+        spreadsheet: {
+          url: process.env.VUE_APP_SUAS_SPREADSHEET,
+          alt: "shut up and sew spreadsheet"
+        }
+      }
+    }
+  },
+  methods: {
+    toggleAbout: function () {
+      this.aboutHidden = !this.aboutHidden;
+    }
+  }
+}
+</script>
